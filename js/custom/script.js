472d0cd77f143f3e574a0d28c51e4851d47a7bf4
Burcu Taş <burcutas636@gmail.com>
2023-09-09 14:18:31 +0300

    search screen edited

---
+++ js/custom/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.bi-search').on('click', function () {
+        $('.search-model').fadeIn(400);
+        $('.offcanvas.show').css('visibility', 'hidden');
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+        $('.offcanvas.show').css('visibility', 'visible');
+    });
+});
+
+
